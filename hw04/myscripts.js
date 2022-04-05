d157aacc23d2d3506f5dcbcfd91c81c5912445fd
Brunodapic <44134722+Brunodapic@users.noreply.github.com>
2022-04-05 19:30:18 +0200

    finalni produkt

--- hw04/myscripts.js
+++ hw04/myscripts.js
@@ -1,13 +1,5 @@
 
-
-console.log("yes")
-
-
-
 const url = 'https://pokeapi.co/api/v2/pokemon'
-
-
-console.log('Fetch started')
 
 
 fetch(url)
@@ -23,22 +15,30 @@ fetch(url)
                 console.log(decodedData.results)
                 const list = document.getElementById("portfolio");
                 for (let i = 0; i < decodedData.results.length; i++) {
-                    console.log(decodedData.results[i].url)
                     uri = decodedData.results[i].url
                     uri = uri.split('/')
                     uri = uri[uri.length - 2];
                     getPokemon(uri)
-                    console.log(localStorage.getItem(uri))
+                    //getPokomonType(uri)
                     list.innerHTML +=
-                        `<div class="img-box">
-                        <img class="project__image" src=${"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/" + uri + ".png"}>
-                        <div class="transparent-box">
-                            <div class="caption">
-                                <p>${decodedData.results[i].name}</p>
-                                <p class="opacity-low" id=${uri}></p>
+                        `
+                        <div class="window">
+                            <div id="${uri}"   class="img-box">
+                                <img  class="project__image" src=${"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/" + uri + ".png"}>
+                                <div class="transparent-box">
+                                    <div class="caption">
+                                        <p>${decodedData.results[i].name.toUpperCase()}</p>
+                                        <p class="opacity-low" id=${"name_"+uri}></p>
+                                    </div>
+                                </div>
                             </div>
-                        </div> 
-                     </div>`;
+                            <div class="overlay">
+                                <button onclick="myFunction(${uri})" >
+                                    <img id="ball_${uri}" class="overlay_image" src="ball_open.png">
+                                </button>
+                            </div>
+                            
+                        </div>`;
                 }
             })
         } else {
@@ -57,10 +57,10 @@ async function getPokemon(id) {
         .then(response => {
             if (response.status === 200) {
                 response.json().then(decodedData => {
-                    console.log(decodedData)
-                    localStorage.setItem(id, decodedData);
-                    const p=document.getElementById(id)
-                    p.innerHTML=decodedData.species.name
+                    //localStorage.setItem(id, decodedData);
+                    const p = document.getElementById("name_"+id)
+                    console.log(decodedData.types[0].type.name)
+                    p.innerHTML = decodedData.types[0].type.name
                 })
             } else {
                 console.log('Response status code is not OK!')
@@ -69,6 +69,49 @@ async function getPokemon(id) {
         .catch(console.error)
         // finally will be always called when promise finished (`then` or `catch`)
         .finally(() => {
-            console.log('Fetch finished!')
+            //console.log('Fetch finished!')
         })
-    }
+}
+
+
+
+function myFunction(id) {
+    const targetDiv = document.getElementById(id);
+    const targetImg=document.getElementById("ball_"+id)
+     
+    if (targetDiv.style.display === "none") {
+        targetDiv.style.display = "block";
+        targetImg.src="ball_open.png"
+
+      } else {
+        targetDiv.style.display = "none";
+        targetImg.src="ball.png"
+
+      }
+    console.log(id)
+}
+
+//ništa ne radi funkcija , imao sam jednu ideju ,ali previše sam vremena proveo na oveme pa sam odustao
+
+/*async function getPokomonType(id) {
+    fetch("https://pokeapi.co/api/v2/gender/" + id + "/")
+        .then(response => {
+            if (response.status === 200) {
+                response.json().then(decodedData => {
+                    //localStorage.setItem(id, decodedData);
+                    const p = document.getElementById("name_"+id)
+                    console.log(decodedData)
+                    p.innerHTML = decodedData.name
+                })
+            } else {
+                const p = document.getElementById("name_"+id)
+                p.innerHTML = "unknown"
+                console.log('Response status code is not OK!')
+            }
+        })
+        .catch(console.error)
+        // finally will be always called when promise finished (`then` or `catch`)
+        .finally(() => {
+            //console.log('Fetch finished!')
+        })
+}*/
