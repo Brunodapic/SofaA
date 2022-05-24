a5715f69cb900516fe27e528688afd8c6b2447eb
Brunodapic <44134722+Brunodapic@users.noreply.github.com>
2022-05-24 19:33:42 +0200

    pocetna faza

---
+++ hw09/sofa_projekt/modules/Player/components/PlayerHeader.tsx
@@ -0,0 +1,12 @@
+import Link from 'next/link'
+import React from 'react'
+import { FullPlayer } from '../../../model/Player'
+
+export default function PlayerHeader({player}: {player: FullPlayer}) {
+  console.log(player)
+  return (
+    <>
+      <div>{player.name}</div>
+    </>
+  )
+}
--- hw09/sofa_projekt/pages/_app.tsx
+++ hw09/sofa_projekt/pages/_app.tsx
@@ -1,8 +1,25 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import React from 'react'
+import { SWRConfig, SWRConfiguration } from 'swr';
+import fetcher from '../util/fetch'
+
+const swrConfig: SWRConfiguration = { fetcher }
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+
+  const isServer = typeof window === undefined
+  
+  React.useEffect(() => {
+    console.log('log')
+    console.log(isServer)
+  }, [isServer])
+
+  return (
+    <SWRConfig value={swrConfig}>
+      <Component {...pageProps} />
+    </SWRConfig>
+  )
 }
 
 export default MyApp
